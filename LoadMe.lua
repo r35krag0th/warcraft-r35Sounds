08f6e6b471d180150e959a01793b3a30c473c5c9
Bob Saska <git@r35.net>
2011-02-04 20:27:07 -0600

    Initial files and configuration stuff

---
+++ LoadMe.lua
@@ -0,0 +1,13 @@
+--
+-- Extra sounds I like for alerts.  Mostly for my PowerAuras.
+--
+
+local lsm = LibStub("LibSharedMedia-3.0")
+
+LSM:Register("sound", "r35 Train",          [[Interface\Addons\r35Sounds\media\train1.mp3]])
+LSM:Register("sound", "r35 Appear",         [[Interface\Addons\r35Sounds\media\appear.mp3]])
+LSM:Register("sound", "r35 Button Press 3", [[Interface\Addons\r35Sounds\media\buttonPress3.mp3]])
+LSM:Register("sound", "r35 Button Press 4", [[Interface\Addons\r35Sounds\media\buttonPress4.mp3]])
+LSM:Register("sound", "r35 Indian Bell",    [[Interface\Addons\r35Sounds\media\indianBell.mp3]])
+LSM:Register("sound", "r35 powerUp", [[Interface\Addons\r35Sounds\media\powerUp.mp3]])
+LSM:Register("sound", "r35 robotBlip", [[Interface\Addons\r35Sounds\media\robotBlip.mp3]])
